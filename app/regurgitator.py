6716a9c1b848f87eddf5f14d4c1b29c67f36d6cf
mark-curran <mjcur_11@hotmail.com>
2022-09-23 01:47:40 +0000

    Lint regurgitator.

--- app/regurgitator.py
+++ app/regurgitator.py
@@ -1,8 +1,13 @@
+import numpy as np
+
+
 class Regurgitator:
-    
     def __init__(self):
         self.initiated = True
 
-    def hello(self,name:str) -> str:
-
+    def hello(self, name: str) -> str:
         return f"Hello, {name}!"
+
+    def goodbye(self, name: str) -> str:
+
+        return f"Goodbye, {name} :("
--- tests/test_Regurgitator.py
+++ tests/test_Regurgitator.py
@@ -2,15 +2,23 @@ import pytest
 
 from app.regurgitator import Regurgitator
 
+
 @pytest.fixture
 def regurgitate():
 
     return Regurgitator()
 
+
 def test_init(regurgitate):
 
     assert regurgitate.initiated
 
+
 def test_hello(regurgitate):
 
-    assert regurgitate.hello("Mark") == "Hello, Mark!"
+    assert regurgitate.hello("Bob") == "Hello, Bob!"
+
+
+def test_goodbye(regurgitate):
+
+    print("hello")
