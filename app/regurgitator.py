5bdda0f6d28740e9e18315aa3aa67fe36a047396
mark-curran <mjcur_11@hotmail.com>
2022-09-23 06:07:35 +0000

    remove import np from regurgitator.

--- app/regurgitator.py
+++ app/regurgitator.py
@@ -1,11 +1,10 @@
-import numpy as np
-
-
 class Regurgitator:
     def __init__(self):
+
         self.initiated = True
 
     def hello(self, name: str) -> str:
+
         return f"Hello, {name}!"
 
     def goodbye(self, name: str) -> str:
